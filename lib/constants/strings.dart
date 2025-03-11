e55bcf1bf80e1143770002067891ecfd96cdf9b8
teepakornLT <teeprakorn.toey@gmail.com>
2025-03-11 09:07:07 +0700

    VERSION 1.0.0

--- lib/constants/strings.dart
+++ lib/constants/strings.dart
@@ -2,7 +2,7 @@ class AppStrings {
   static const String appName = 'PM2.5 App';
 
   //API Path
-  static const String urls = 'http://10.13.3.108:3000';
+  static const String urls = 'http://102.211.232.170:3000';
   static const String apiGetAir4thai = '$urls/api/get-air4thai';
   static const String apiGetAir4thaiStationId = '$urls/api/get-air4thai-station-id';
   static const String apiGetTopPredictions1 = '$urls/api/get-top-predictions-1';
